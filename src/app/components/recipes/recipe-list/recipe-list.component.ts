f78676bf5010c0f7771f7b82d342bc4281bdfc29
Bárbara Souza Amaral <barbara.amaral@ibm.com>
2022-05-05 13:09:47 -0300

    recipe details/list,  shopping-list/edit e models
    Implementando:
    
    -Recipe details
    -Recipe list
    -Shopping edit
    -Shopping list
    - Models: Ingredient e Recipe

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'a simple test', 'https://cdn.pixabay.com/photo/2016/04/01/09/52/cartoon-1299636_960_720.png'),
+    new Recipe('test2', 'another test', 'https://cdn.pixabay.com/photo/2016/04/01/09/52/cartoon-1299636_960_720.png')
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
